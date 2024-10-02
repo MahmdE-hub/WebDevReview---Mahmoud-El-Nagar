477e430eefe9b791152d66aa9fed28afa7408988
MahmdE-hub <mahmoudbusiness139@gmail.com>
2024-10-02 12:01:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').innerHTML = 'The text has been changed!';
+});
+document.querySelector('#changeTextButton2').addEventListener('click',function(){
+    document.querySelector('#dynamicText2').innerHTML = 'HELLO!';
+
+});
